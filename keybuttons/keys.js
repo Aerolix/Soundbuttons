5baafadb21fc30f4aa39612d3d7f3025572d06e6
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-05-12 09:11:59 -0500

    soup

--- keybuttons/keys.js
+++ keybuttons/keys.js
@@ -1,8 +1,8 @@
 function amazing() {
     window.addEventListener('keydown', function (e) {
      if (e.key == "q") {
-        const sound = new Audio('adf.wav');
-        music.play();
+        const sound = new Audio('keybuttons/Android-Sound-Effect-Meme.mp3');
+        sound.play();
      }
    }, false); 
  }
