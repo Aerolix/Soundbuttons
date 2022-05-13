2c0cfe483491ff793cbfaf79c695d043638b8571
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-05-13 11:18:21 -0500

    s

--- keybuttons/keys.js
+++ keybuttons/keys.js
@@ -45,5 +45,14 @@ function amazing() {
       setTimeout(() => { key.style.backgroundColor = 'white'; }, 500);
       setTimeout(() => { key.style.color = 'black'; }, 500);
     }
+    if (e.key == "y") {
+      var sound = document.getElementById("cola");
+      sound.play();
+      var key = document.getElementById("they");
+      key.style.backgroundColor = 'black';
+      key.style.color = 'white';
+      setTimeout(() => { key.style.backgroundColor = 'white'; }, 500);
+      setTimeout(() => { key.style.color = 'black'; }, 500);
+    }
   }, false); 
 }
