b29c475cdb629c039c40ec33d1f38b4174243968
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-05-18 10:13:25 -0500

    we love the earth

--- secret.js
+++ secret.js
@@ -14,7 +14,7 @@ function correct() {
 
     document.createElement(<iframe width="110" height="200" src="https://www.myinstants.com/instant/cole-ryan-my-mom-43021/embed/" frameborder="0" scrolling="no"></iframe>);
     document.createElement(<iframe width="110" height="200" src="https://www.myinstants.com/instant/cole-ryan-rehehehe-28922/embed/" frameborder="0" scrolling="no"></iframe>);
-    document.createElement(<iframe width="110" height="200" src="https://www.myinstants.com/instant/cole-ryan-wet-my-whistle-37694/embed/" frameborder="0" scrolling="no"></iframe>)
+    document.createElement(<iframe width="110" height="200" src="https://www.myinstants.com/instant/cole-ryan-wet-my-whistle-37694/embed/" frameborder="0" scrolling="no"></iframe>);
 }
 
  
