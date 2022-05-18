94fbeb40e2f136c02231213cb3707f179485aae8
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-05-18 10:14:23 -0500

    s

--- secret.js
+++ secret.js
@@ -1,12 +1,3 @@
-  window.addEventListener('keydown', function (e) {
-      if (e.key == "c")  {
-          var code = this.prompt("Secret Code");
-          if (code = "cole") {
-            correct();
-        }
-      }
-   }, false); 
-
 function correct() {
     document.write(
         <h1>Cole-Ryan mode</h1>
@@ -16,5 +7,15 @@ function correct() {
     document.createElement(<iframe width="110" height="200" src="https://www.myinstants.com/instant/cole-ryan-rehehehe-28922/embed/" frameborder="0" scrolling="no"></iframe>);
     document.createElement(<iframe width="110" height="200" src="https://www.myinstants.com/instant/cole-ryan-wet-my-whistle-37694/embed/" frameborder="0" scrolling="no"></iframe>);
 }
+
+window.addEventListener('keydown', function (e) {
+    if (e.key == "c")  {
+        var code = this.prompt("Secret Code");
+        if (code = "cole") {
+          correct();
+      }
+    }
+ }, false); 
+
 
  
