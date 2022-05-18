6c0a6aa4ef2d79db13986026fa8622b899831212
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-05-18 10:46:26 -0500

    s

--- secret.js
+++ secret.js
@@ -1,17 +1,2 @@
-function correct() {
-    document.write(
-        <h1>Cole-Ryan mode</h1>
-    );
 
-    document.createElement(<iframe width="110" height="200" src="https://www.myinstants.com/instant/cole-ryan-my-mom-43021/embed/" frameborder="0" scrolling="no"></iframe>);
-    document.createElement(<iframe width="110" height="200" src="https://www.myinstants.com/instant/cole-ryan-rehehehe-28922/embed/" frameborder="0" scrolling="no"></iframe>);
-    document.createElement(<iframe width="110" height="200" src="https://www.myinstants.com/instant/cole-ryan-wet-my-whistle-37694/embed/" frameborder="0" scrolling="no"></iframe>);
-}
-
-function ask() {
-    var code = this.prompt("Secret Code");
-    if (code = "cole") {
-  correct();
-}
-}
 
