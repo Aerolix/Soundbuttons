5f6cd581a568e289b4b0d6f995c908eb91c5cc07
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-05-18 10:02:04 -0500

    code

--- secret.js
+++ secret.js
@@ -1,21 +1,15 @@
   window.addEventListener('keydown', function (e) {
-        if (e.key == "c") {
-            var cole = "c";
-            if (e.key == "o") {
-                var cole = "co";
-                if (e.key == "l") {
-                    var cole = "col";
-                    if (e.key == "e") {
-                        var cole = "cole";
-                        run();
-                    }
-                }
-            }
-        }
+      if (e.key == "c")  {
+          var code = this.prompt("Secret Code")
+      }
    }, false); 
 
-function run() {
-    alert("it workd")
+if (code = "cole") {
+    correct();
+}
+
+function correct() {
+    alert(("good"))
 }
 
  
