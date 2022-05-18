121fc18c787472fbabd6b775696ca94f764638a5
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-05-18 10:12:46 -0500

    s

--- secret.js
+++ secret.js
@@ -8,7 +8,13 @@
    }, false); 
 
 function correct() {
-    alert(("good"))
+    document.write(
+        <h1>Cole-Ryan mode</h1>
+    );
+
+    document.createElement(<iframe width="110" height="200" src="https://www.myinstants.com/instant/cole-ryan-my-mom-43021/embed/" frameborder="0" scrolling="no"></iframe>);
+    document.createElement(<iframe width="110" height="200" src="https://www.myinstants.com/instant/cole-ryan-rehehehe-28922/embed/" frameborder="0" scrolling="no"></iframe>);
+    document.createElement(<iframe width="110" height="200" src="https://www.myinstants.com/instant/cole-ryan-wet-my-whistle-37694/embed/" frameborder="0" scrolling="no"></iframe>)
 }
 
  
