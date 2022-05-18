005080aa23806ef2b30b5b223bcb223bbc9b7e94
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-05-18 08:07:41 -0500

    hs

---
+++ secret.js
@@ -0,0 +1,13 @@
+function amazing() {
+    window.addEventListener('keydown', function (e) {
+        if (e.key == 'c') {
+            if (e.key == 'o') {
+                if (e.key == 'l') {
+                    if (e.key == 'e') {
+                        this.alert("pressed cole")
+                    }
+                }
+            }
+        }
+   }, false); 
+ }
