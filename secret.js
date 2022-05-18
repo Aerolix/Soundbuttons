4ceb291ba622e1f1c4ff114cf6370580035c4016
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-05-18 08:14:15 -0500

    s

--- secret.js
+++ secret.js
@@ -1,8 +1,8 @@
  window.addEventListener('keydown', function (e) {
-        if (e.key == 'c') {
-            if (e.key == 'o') {
-                if (e.key == 'l') {
-                    if (e.key == 'e') {
+        if (e.key == "c") {
+            if (e.key == "o") {
+                if (e.key == "l") {
+                    if (e.key == "e") {
                         sus()
                     }
                 }
